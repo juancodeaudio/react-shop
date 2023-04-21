1d3e32a570d6542f0a6e6fec4e8897d324f555bd
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2023-04-20 22:30:56 -0500

    fix: :bug: https

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -2,7 +2,7 @@ import { useGetProducts } from "../hooks/useGetProducts";
 import { ProductItem } from "../components/ProductItem";
 import "../styles/ProductList.scss";
 
-const API = "http://api.escuelajs.co/api/v1/products";
+const API = "https://api.escuelajs.co/api/v1/products";
 
 function ProductList() {
   const products = useGetProducts(API);
